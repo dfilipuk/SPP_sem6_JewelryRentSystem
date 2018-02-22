f217ede82c1727ef0c3f8d0bbd38918e7ef20bde
Dzmitry Filipuk <dzmitry@filipuk.me>
2018-02-22 19:45:19 +0300

    thymeleaf & jpa-data added

---
+++ jewelry-rent-system/src/main/java/by/bsuir/spp/jewelryrentsystem/model/Jewelry.java
@@ -0,0 +1,74 @@
+package by.bsuir.spp.jewelryrentsystem.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "jewelries")
+public class Jewelry {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Getter
+    @Setter
+    @Column(name = "id", nullable = false)
+    private long id;
+
+    @Column(name = "name")
+    @Getter
+    @Setter
+    private String name;
+
+    @Column(name = "producer")
+    @Getter
+    @Setter
+    private String producer;
+
+    @Column(name = "description")
+    @Getter
+    @Setter
+    private String description;
+
+    @Column(name = "pictureUrl")
+    @Getter
+    @Setter
+    private String pictureUrl;
+
+    @Column(name = "type")
+    @Getter
+    @Setter
+    private String type;
+
+    @Column(name = "weight")
+    @Getter
+    @Setter
+    private double weight;
+
+    @Column(name = "status")
+    @Getter
+    @Setter
+    private String status;
+
+    @Column(name = "cost_per_day")
+    @Getter
+    @Setter
+    private double costPerDay;
+
+    @Column(name = "days_rental")
+    @Getter
+    @Setter
+    private int daysRental;
+
+    @Getter
+    @Setter
+    @ManyToOne
+    @JoinColumn(name = "branch_id", nullable = false)
+    private Branch branch;
+
+    @Getter
+    @Setter
+    @ManyToMany
+    private Set<Material> materials;
+}
