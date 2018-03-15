aca26ee01404bb6bc91147d2803a4fc124458da9
Dmitry Filipuk <kupilifds@gmail.com>
2018-03-15 18:23:53 +0300

    added material entity crud

---
+++ jewelry-rent-system/src/main/java/by/bsuir/spp/jewelryrentsystem/repository/MaterialRepository.java
@@ -0,0 +1,7 @@
+package by.bsuir.spp.jewelryrentsystem.repository;
+
+import by.bsuir.spp.jewelryrentsystem.model.Material;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MaterialRepository extends JpaRepository<Material, Long> {
+}
