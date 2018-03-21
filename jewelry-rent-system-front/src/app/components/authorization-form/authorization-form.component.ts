91572b99506d8ca1f970ddf307939a0a9aa964c5
tanya <t.evtushenko.s@yandex.by>
2018-03-21 23:26:57 +0300

    Add nav, footer and routing to pages

---
+++ jewelry-rent-system-front/src/app/components/authorization-form/authorization-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-authorization-form',
+  templateUrl: './authorization-form.component.html',
+  styleUrls: ['./authorization-form.component.css']
+})
+export class AuthorizationFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
