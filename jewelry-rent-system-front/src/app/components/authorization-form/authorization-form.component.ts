f89efd99e8d589bacdf0172afd66f27b4af0763e
tanya <t.evtushenko.s@yandex.by>
2018-04-07 03:09:32 +0300

    View so content part that is available for current user with certain site access

--- jewelry-rent-system-front/src/app/components/authorization-form/authorization-form.component.ts
+++ jewelry-rent-system-front/src/app/components/authorization-form/authorization-form.component.ts
@@ -1,27 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Injectable } from '@angular/core';
 
 import { Credential } from '../../models/credential';
-import { IAuthorizationService } from '../../services/interfaces/auth-service-interface';
 import { AuthorizationService } from '../../services/auth-service';
+import { UserService } from '../../services/user-service';
 
 @Component({
   selector: 'authorization-form',
   templateUrl: './authorization-form.component.html',
-  styleUrls: ['./authorization-form.component.css'],
-  providers: [AuthorizationService]
+  styleUrls: ['./authorization-form.component.css']
 })
+@Injectable()
 export class AuthorizationFormComponent implements OnInit {
 
   loginData: Credential = new Credential();
 
-  constructor(private service: AuthorizationService) { }
+  constructor(private service: AuthorizationService, public currentUser: UserService) { }
 
   ngOnInit() {
   }
 
   signIn() {
-    this.service.login(this.loginData).subscribe(data => {
-      console.log(data);
+    this.service.login(this.loginData).then(() => {
+      this.loginData = new Credential();
     });
   }
 
