e745cb516543528443580059c7ee54b1a3471e51
tanya <t.evtushenko.s@yandex.by>
2018-04-20 23:38:25 +0300

    Fill order crud component

--- jewelry-rent-system-front/src/app/models/jewelry.ts
+++ jewelry-rent-system-front/src/app/models/jewelry.ts
@@ -1,14 +1,16 @@
 export class Jewelry {
-    id: number;
-    name: string;
-    producer: string;
-    description: string;
-    pictureUrl: string;
-    type: string;
-    weight: number;
-    status: string;
-    costPerDay: number;
-    daysRental: number;
-    branchId: number;
-    materialsIds: number[];
+    constructor(
+        public id: number,
+        public name: string,
+        public producer: string,
+        public description: string,
+        public pictureUrl: string,
+        public type: string,
+        public weight: number,
+        public status: string,
+        public costPerDay: number,
+        public daysRental: number,
+        public branchId: number,
+        public materialsIds: number[]
+    ) { }
 }
